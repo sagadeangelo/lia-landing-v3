a7c75d5ebebf7daa19090b256a1d0c6c851e11c7
SagaDeAngelo <miguel.tovar.amaral@gmail.com>
2026-08-05 10:37:57 -0500

    fix(sprint4.5): remove css import from js to prevent mime type error in prod

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * Importa el sistema de estilos global y arranca LIA_OS.
  */
 
-import './styles/global.css';
+// CSS is now loaded via <link> in index.html
 import { LIA_OS } from './core/LIA_OS.js';
 
 const boot = () => {
